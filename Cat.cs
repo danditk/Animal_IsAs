8a6ec5e5748c03193358ec50f1b6261e84bbeccb
Darek <darekt@abmicro.pl>
2021-11-18 20:53:35 +0100

    Init lesson 34. Create animal class witch is inheritis by animal

---
+++ Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_IsAs
+{
+    class Cat : Animal
+    {
+        public void PijMleko()
+        {
+            Console.WriteLine("Pije mleko...");
+        }
+    }
+}
